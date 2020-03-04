7d5050100c18450cb58698be224072ac7984f994
Madhan Kumaravel <maddymadhan946@gmail.com>
2020-03-04 15:20:03 +0530

    checking loading state of the page

--- wait_for_ajax.rb
+++ wait_for_ajax.rb
@@ -1,11 +1,17 @@
 module WaitForAjax
+
   def wait_for_ajax
     Timeout.timeout(Capybara.default_max_wait_time) do
-      loop until finished_all_ajax_requests?
+      loop until finished_all_ajax_requests? && document_state?
     end
   end
 
   def finished_all_ajax_requests?
     @driver.evaluate_script('jQuery.active').zero?
   end
+
+  def document_state?
+    @driver.evaluate_script("document.readyState === 'complete'")
+  end
+
 end
